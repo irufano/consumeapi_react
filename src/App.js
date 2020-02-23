ac6580197ba42f38cd81bd89c726bca59e6df3de
M. Irfan Hidayat <irfan@multiinti.com>
2020-02-23 21:10:30 +0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          items: data,
+          isLoading: false
+        })
+      );
+  }
+
+  render() {
+    const { items, isLoading } = this.state;
+
+    if (isLoading) {
+      return <h1>Loading ... </h1>;
+    }
+    
+    return (
+      <div>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}> {item.name} </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
